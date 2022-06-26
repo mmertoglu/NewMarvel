86d5fdac5971a3fcab0e005f8810db49883004d4
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-06-26 14:37:44 +0300

    signup page added

---
+++ NewMarvel/src/screens/SignUp/SignUp.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import {View,Text,TouchableOpacity,Image} from 'react-native'
+import styles from './SignUp.style'
+import Input from "../../components/Input/Input";
+import LoginButton from "../../components/LoginButton/LoginButton";
+import auth from '@react-native-firebase/auth'
+import IonIcons from 'react-native-vector-icons/Ionicons'
+const SignUp = ({navigation}) => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const {rePassword,setRePassword} = useState('')
+
+    const handleSignUp = () => {
+        if (email||password||rePassword !='') {
+            if (password ===rePassword) {
+                try {
+                    auth().createUserWithEmailAndPassword(email,password)
+                } catch (error) {
+                    console.log(error)
+                }
+            } else {
+                console.log('aynı olmalı')
+            }
+           
+        } else {
+            console.log('cant be null')
+        }
+    }
+    const goBack  = () => {
+        navigation.goBack();
+    }
+    return(
+        <View style={styles.container} >
+            <IonIcons name="arrow-back" color={'white'} style={styles.back_icon} size={30} onPress={goBack} />
+             <Image source={{ uri: 'https://1000logos.net/wp-content/uploads/2021/06/Marvel-Comics-logo.png' }} style={styles.image} />
+            <View style={styles.input_container} >
+            <Input
+
+                iconName='user'
+                placeHolder='email...'
+                headerText='Email'
+                onChangeText={text => setEmail(text)} />
+                <Input
+                iconName='key'
+                placeHolder='password...'
+                headerText='Password'
+                onChangeText={text => setPassword(text)} />
+                 <Input
+                iconName='key'
+                placeHolder='password...'
+                headerText='Password'
+                onChangeText={text => setRePassword(text)} />
+            </View>
+            <LoginButton
+                onPress={handleSignUp}
+                Button_text='SIGN UP'
+                />
+
+        </View>
+    )
+}
+
+export default SignUp
---
+++ NewMarvel/src/screens/SignUp/SignUp.style.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        alignItems:'center',
+        flex:1,
+        backgroundColor:'red',
+        justifyContent:'center'
+    },
+    image: {
+        width: 300,
+        height:140
+    },
+    input_container:{
+        width:'100%',
+        alignItems:'center',
+        marginBottom:30
+    },
+    account_text:{
+        color:'white',
+        marginTop:50
+    },
+    back_icon:{
+        position:'absolute',
+        top:20,
+        left:20
+    }
+})
