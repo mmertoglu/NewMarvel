aa7cfe641e904afc1974b8a5c695d5539b475523
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-06-26 18:14:14 +0300

    favourites added

--- NewMarvel/src/screens/Favourites/Favourites.js
+++ NewMarvel/src/screens/Favourites/Favourites.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import {View,Text} from 'react-native'
-
+import React,{useEffect,useState} from 'react'
+import {View,Text,FlatList} from 'react-native'
+import database from '@react-native-firebase/database'
+import ParseContent from '../../utils/ParseContent'
+import auth from '@react-native-firebase/auth'
+import StarterCharacterCard from '../../components/StarterCharacterCard/StarterCharacterCard'
+const currentUser = auth().currentUser.uid
 const Favourites = () => {
+    const [data,setData] =useState([])
+    useEffect(() => {
+        database().ref('users/'+currentUser+'/favourites').on('value',snapshot => {
+            const newData = snapshot.val();
+            const ParsedData = ParseContent(newData)
+            setData(ParsedData)
+            console.log(data)
+        })
+    },[])
+    const renderItem = ({item}) => <StarterCharacterCard character={item} handleDetail={as} />
+    const as = () => {
+        
+    }
     return(
         <View>
-            <Text>
-                Favourites
-            </Text>
+            <FlatList
+            data={data}
+            renderItem={renderItem}
+            />
         </View>
     )
 }
