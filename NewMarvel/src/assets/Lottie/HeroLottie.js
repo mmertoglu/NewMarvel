6d54fd6160407757ec3859f57591ea411ba22b9e
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-06-26 17:43:53 +0300

    lottie changed

--- NewMarvel/src/assets/Lottie/HeroLottie.js
+++ NewMarvel/src/assets/Lottie/HeroLottie.js
@@ -3,7 +3,7 @@ import LottieView from 'lottie-react-native'
 
 const HeroLottie = () => {
     return(
-        <LottieView autoPlay source={require('./loading.json')} style={{backgroundColor:'#000080'}} />
+        <LottieView autoPlay source={require('./loading.json')} style={{backgroundColor:'red'}} />
     )
 }
 export default HeroLottie
