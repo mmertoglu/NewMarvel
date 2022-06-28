5377fa8c800a892a8be9fe2d78c58f37f886f4ff
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-06-28 16:21:51 +0300

    modal integration

--- NewMarvel/src/components/FavouriteCard/FavouriteCard.js
+++ NewMarvel/src/components/FavouriteCard/FavouriteCard.js
@@ -2,7 +2,9 @@ import React from "react";
 import {View,Text,Image, TouchableOpacity, Alert} from 'react-native'
 import Entypo from 'react-native-vector-icons/Entypo'
 import styles from './FavouriteCard.style'
-const FavouriteCard = ({character,handleRemove}) => {
+import Modal from 'react-native-modal'
+import RemoveModalComponent from "../RemoveModalComponent/RemoveModalComponent";
+const FavouriteCard = ({character,handleRemove,RemoveItem,CloseModal,isModalVisible}) => {
     return(
         <TouchableOpacity onLongPress={() =>handleRemove(character)} style={styles.container}>
             { character.thumbnail != undefined ? <Image source={{uri:character.thumbnail.path+'.jpg'}} style={styles.image} resizeMode='stretch' />
@@ -11,6 +13,8 @@ const FavouriteCard = ({character,handleRemove}) => {
             <View style={styles.text_container} >
             <Text numberOfLines={1}  style={styles.text} >{character.name}</Text>
             </View>
+            <RemoveModalComponent
+            isModalVisible={isModalVisible} CloseModal={CloseModal} handleRemoveItem={()=>RemoveItem(character)} />
         </TouchableOpacity>
     )
 }
